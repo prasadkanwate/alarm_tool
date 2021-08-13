8d9dba438c01cf57185afdcbf6102edc841121d5
prasadkanwate <63545003+prasadkanwate@users.noreply.github.com>
2021-08-13 21:56:26 +0530

    Add files via upload

---
+++ alarm.py
@@ -0,0 +1,80 @@
+from tkinter import*
+from tkinter import ttk
+import time
+import random;
+import os
+from tkinter import messagebox
+
+root=Tk()
+root.geometry("700x350+0+0")
+root.title("Alarm tool")
+              
+###Creating the frame#######
+top = Frame(root, width = 700, height = 20, relief = SUNKEN)
+top.pack(side = TOP)
+
+left=Frame(root,width=350,height=250,relief=SUNKEN)
+left.pack(side=LEFT)
+
+#####creating label of title######
+
+label_info=Label(top,font=('arial',40,'bold'),text="Alarm Tool",fg = "steel blue", bd = 10, anchor = 'w')
+label_info.grid(row=0,column=0)
+
+local_time = time.asctime(time.localtime(time.time()))
+
+label_info = Label(top, font = ('arial',10,'bold'), text = local_time, fg = "steel blue", bd = 10, anchor = 'w')
+label_info.grid(row = 1, column = 0)
+
+#def main():####
+
+def SubmitButton():
+  AlarmTime= entry1.get()
+  Message1()
+  #label2.config(text ="The Alarm will Ring at {} ".format(AlarmTime))  #delayed in execution
+  CurrentTime = time.strftime("%H:%M")
+  print("the alarm time is: {}".format(AlarmTime))
+  #label2.config(text="")
+  while AlarmTime != CurrentTime:
+    #label2.config(text ="The Alarm will Ring at {} ".format(AlarmTime))
+    CurrentTime = time.strftime("%H:%M")
+    time.sleep(1)   
+  if AlarmTime == CurrentTime:
+     print("now Alarm Musing Playing")
+     os.system("start Wecker-sound.mp3")
+     label2.config(text = "Alarm music playing.....")
+     messagebox.showinfo(title= 'Alarm Message', message= "{}".format(entry2.get()))
+def Message1():
+    AlarmTimeLable= entry1.get()
+    label2.config(text="the Alarm time is Counting...")
+    #label2.config(text= "the Alarm will ring at {}".format(AlarmTimeLable))
+    messagebox.showinfo(title = 'Alarm clock', message = 'Alarm will Ring at {}'.format(AlarmTimeLable))     
+frame1 = ttk.Frame(root)
+frame1.pack()
+frame1.config(height = 100, width = 100,)
+
+label1= ttk.Label(frame1,text = "Enter the Alarm time :")
+label1.pack()
+
+
+entry1 = ttk.Entry(frame1, width = 30)
+entry1.pack()
+entry1.insert(3,"example - 13:15")
+
+labelAlarmMessage= ttk.Label(frame1, text="Alarm Message:")
+labelAlarmMessage.pack()
+
+entry2= ttk.Entry(frame1, width=30)
+entry2.pack()
+
+button1= ttk.Button(frame1, text= "submit", command=SubmitButton)
+button1.pack()
+#this Label2 will show the Last Alarm Time
+label2= ttk.Label(frame1)
+label2.pack()
+
+    
+#label2.config(text="hello")
+
+root.mainloop()
+
